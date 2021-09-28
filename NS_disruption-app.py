7a628fb63e0b21ef999322e3c9a83040f6ac494c
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-09-28 20:31:35 +0200

    Update NS_disruption-app.py

--- NS_disruption-app.py
+++ NS_disruption-app.py
@@ -8,9 +8,13 @@ import plotly.express as px
 st.title('NS disruptions van 24/09 - 26/09')
 
 st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-* **Python libraries:** base64, pandas, streamlit, yfinance, numpy, matplotlib
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+We zijn studenten van de minor Data Science. Ons is gevraagd een app te produceren van een zelfgekozen API. We hebben hiervoor het internet 
+afgestruind en kwamen uiteindelijk terecht bij een gezamelijk factor: **de Nederlandse Spoorwegen (NS)**. Wij hebben gebruik gemaakt van de gegevens 
+van de NS-app van de top 10 grootste stations (Grootste treinstations Nederland, n.d.). Hieruit hebben we de volgende APIs gebruikt: 'places' en 
+'reisinformatie'. Uit deze twee API hebben we 'GET list places' en 'GET disruptions' gebruikt. 
+
+Vervolgens hebben we de informatie van **vrijdag (24-09-2021)**, **zaterdag (25-09-2021)** en **zondag (26-09-2021)** opgehaald en opgeslagen in een 
+dataframe, waarna we in deze app verschillende visualisaties hebben gemaakt. 
 """)
 
 st.sidebar.header('User Input Features')
