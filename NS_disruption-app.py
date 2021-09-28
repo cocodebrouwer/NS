b3dc7e177efe7bc83c4af86ef5e8c4701cebdcf9
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-09-28 20:16:14 +0200

    Update NS_disruption-app.py

--- NS_disruption-app.py
+++ NS_disruption-app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import base64
 import matplotlib.pyplot as plt
 import numpy as np
-import yfinance as yf
+import json,requests
+import plotly.express as px
 
 st.title('S&P 500 App')
 
